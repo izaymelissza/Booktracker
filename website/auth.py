0067613c4c2e8f202646243ab421f267a2230c3d
izaymelissza <melissaizaj@gmail.com>
2025-11-08 15:55:31 +0100

    Creating all necessary routes, basic styling with bootstrap

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-#@auth.route('/')
-#def 
+@auth.route('/login')
+def login():
+    return render_template("login.html", text="Testing")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
+
+@auth.route('/books')
+def books():
+    return "<p>Books</p>"
+
+@auth.route('/reading-lists')
+def reading_lists():
+    return "<p>reading lists</p>"
+
+@auth.route('/stats')
+def stats():
+    return "<p>Stats</p>"
