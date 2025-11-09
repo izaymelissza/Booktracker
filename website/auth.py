b3b7360732536c11f144556c4b189895b223146c
izaymelissza <melissaizaj@gmail.com>
2025-11-09 13:21:46 +0100

    finishing login and sign up logic

--- website/auth.py
+++ website/auth.py
@@ -1,44 +1,67 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing")
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Username does not exist.', category='error')
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
+        username = request.form.get('username')
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user_email = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
+
+        if user_email:
+            flash('There is an account already with this email', category='error')
+        elif user:
+            flash('Username already exists.', category='error')
+        elif len(email) < 4:
             flash('Email is not the standard format.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters', category='error')
         else:
+            new_user = User(username=username, email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
-    
-    return render_template("sign_up.html")
-
-@auth.route('/books')
-def books():
-    return "<p>Books</p>"
-
-@auth.route('/reading-lists')
-def reading_lists():
-    return "<p>reading lists</p>"
-
-@auth.route('/stats')
-def stats():
-    return "<p>Stats</p>"
+            return redirect(url_for('views.home'))
+     
+    return render_template("sign_up.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,9 +1,22 @@
 from flask import Blueprint, render_template
-
+from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
 
+@views.route('/books')
+@login_required
+def books():
+    return "<p>Books</p>"
 
+@views.route('/reading-lists')
+@login_required
+def reading_lists():
+    return "<p>reading lists</p>"
+
+@views.route('/stats')
+@login_required
+def stats():
+    return "<p>Stats</p>"
